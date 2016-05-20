6ccd809be533c9af7b8f6924d1a8d4e717670c4f
zhalfa <zhalfa@hotmail.com>
2016-05-20 16:01:29 +0800

    Project fin

--- credit card/code.R
+++ credit card/code.R
@@ -1,5 +1,5 @@
 rm(list=ls(all=TRUE));
-#dcredit = read.csv("test.csv")
+#dcredit = read.csv("data-r.csv")
 dcredit = read.csv("default of credit card clients.csv");
 #check sample count
 dim(dcredit)
@@ -34,6 +34,32 @@ library(MASS)
 	dcredit$SEX <- factor(dcredit$SEX);
 	dcredit$EDUCATION<- factor(dcredit$EDUCATION);
 	dcredit$MARRIAGE<- factor(dcredit$MARRIAGE);
+
+if(FALSE){
+	dcredit$PAY_0<- factor(dcredit$PAY_0);
+	dcredit$PAY_2<- factor(dcredit$PAY_2);
+	dcredit$PAY_3<- factor(dcredit$PAY_3);
+	dcredit$PAY_4<- factor(dcredit$PAY_4);
+	dcredit$PAY_5<- factor(dcredit$PAY_5);
+	dcredit$PAY_6<- factor(dcredit$PAY_6);
+}
+	dcredit$LIMIT_BAL <- dcredit$LIMIT_BAL/10000;
+if(TRUE){
+	dcredit$BILL_AMT1<- dcredit$BILL_AMT1/1000;
+	dcredit$BILL_AMT2<- dcredit$BILL_AMT2/1000;
+	dcredit$BILL_AMT3<- dcredit$BILL_AMT3/1000;
+	dcredit$BILL_AMT4<- dcredit$BILL_AMT4/1000;
+	dcredit$BILL_AMT5<- dcredit$BILL_AMT5/1000;
+	dcredit$BILL_AMT6<- dcredit$BILL_AMT6/1000;
+
+
+	dcredit$PAY_AMT1<- dcredit$PAY_AMT1/1000;
+	dcredit$PAY_AMT2<- dcredit$PAY_AMT2/1000;
+	dcredit$PAY_AMT3<- dcredit$PAY_AMT3/1000;
+	dcredit$PAY_AMT4<- dcredit$PAY_AMT4/1000;
+	dcredit$PAY_AMT5<- dcredit$PAY_AMT5/1000;
+	dcredit$PAY_AMT6<- dcredit$PAY_AMT6/1000;
+}
 }
 
 str(dcredit);
@@ -49,14 +75,17 @@ trainData <- dcredit[ind==1,]
 testData <- dcredit[ind==2,]
 
 #mode
+#1
 myFormula <- DEFAULT_NEXT~ LIMIT_BAL + SEX + EDUCATION + MARRIAGE + AGE + PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
+#2
+testFormula <- DEFAULT_NEXT~ LIMIT_BAL + EDUCATION + PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
+#3
+test2Formula <- DEFAULT_NEXT~ PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
 
 #tree
 library(party)
-fun_tree <- function(){
+fun_tree <- function( treeFormula ){
 	print("decision tree");
-	treeFormula <- DEFAULT_NEXT~ LIMIT_BAL + SEX + EDUCATION + MARRIAGE + AGE + PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
-
 	credit_tree <- ctree(treeFormula, data=trainData)
 	table(trainData$DEFAULT_NEXT, predict(credit_tree))
 
@@ -64,27 +93,29 @@ fun_tree <- function(){
 	table(testData$DEFAULT_NEXT, treePred)
 }
 
+library(rpart);
+fun_rp_tree <- function(){
+	rpart_tree <- rpart(myFormula,trainData);
+	rp_pre <- predit( rpart_tree, testData);
+}
+
+
 #nnet
 library(nnet)
-fun_nnet <- function(){
+fun_nnet <- function( nnetFormula ){
 	print("nnet");
-	nnetFormula <- DEFAULT_NEXT ~ LIMIT_BAL + SEX + EDUCATION + MARRIAGE + AGE + PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
-
-	credit_nnt<- nnet(nnetFormula, data = trainData,  size=5, rang =0.1, decay=5e-4,maxit=500);
+	credit_nnt<- nnet(testFormula, data = trainData,  size=5, rang =0.1, decay=5e-4,maxit=500);
 	table(trainData$DEFAULT_NEXT, predict(credit_nnt));
 
-	nntPred <- predict(credit_nnt, testData);
+	nntPred <- predict(credit_nnt, testData, type ="class");
 	table(testData$DEFAULT_NEXT, nntPred);
 }
 
 #svm
 library(e1071)
-
-fun_svm <- function(){
+fun_svm <- function( svmFormula ){
 	print("svm");
-	svmFormula <- DEFAULT_NEXT~ LIMIT_BAL + SEX + EDUCATION + MARRIAGE + AGE + PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6
-
-	credit_svm <- svm(svmFormula, data=trainData);
+	credit_svm <- svm(svmFormula, data=trainData );
 	table(trainData$DEFAULT_NEXT, predict(credit_svm));
 
 	svmPre <- predict(credit_svm,testData);
